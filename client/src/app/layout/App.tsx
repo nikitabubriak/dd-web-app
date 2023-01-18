08d18752dabc6f07bacb7285d82b5aaa49b352dc
Nikita Bubriak <31805409+nikitabubriak@users.noreply.github.com>
2023-01-18 13:49:03 +0200

    create basic client

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { Product } from "../models/product";
+
+function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, [])
+
+  return (
+    <div>
+      <h1>DD Web App</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} - {product.price}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
